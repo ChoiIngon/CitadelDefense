073a11d245cf29bbf95a5c7bf2388c845cb97d01
ChoiIngon <kukuta@gmail.com>
2016-05-30 11:29:54 +0900

    이상한 코드들 다 지워버리고 새로 만들어 보장

--- Assets/BehaviourTree/Source/NodeCanvas.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class NodeCanvas : ScriptableObject
-{
-    public List<Node> nodes = new List<Node>();
-}
