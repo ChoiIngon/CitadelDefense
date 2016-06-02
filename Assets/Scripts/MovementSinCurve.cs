2a328a7bf83694c9fd2ce94d50f6ade9f57962c5
kukuta <kukuta@gmail.com>
2016-06-03 01:59:24 +0900

    캐릭터 공격
    만드는 중...

---
+++ Assets/Scripts/MovementSinCurve.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementSinCurve : MonoBehaviour {
+
+	public float speed;
+	public Vector3 start;
+	public Vector3 end;
+	public float height;
+	private float interpolate;
+	private float distance;
+
+	public void Init(Vector3 start, Vector3 end)
+	{
+		this.start = start;
+		this.end = end;
+	}
+
+	void Start()
+	{
+		distance = Vector3.Distance (start, end);
+	}
+
+	void Update() {
+		interpolate += Time.deltaTime * speed / distance;
+		Vector3 curPos = Vector3.Lerp (start, end, interpolate);
+		curPos.y += Mathf.Sin (Mathf.PI * interpolate) * height;
+		transform.position = curPos;
+		if (1.0f <= interpolate) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MovementSinCurve movement = gameObject.AddComponent<MovementSinCurve> ();
+		movement.start = new Vector3 (-10, 0, 0);
+		movement.end = new Vector3 (10, 0, 0);
+		movement.speed = 5;
+		movement.height = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
