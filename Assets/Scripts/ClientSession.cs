ced1c60b788bd44cbfc038bf648513260b967b8a
ChoiIngon <kukuta@gmail.com>
2016-06-02 19:00:14 +0900

    Network
    네트워크 스트레스 테스트 코드 추가

---
+++ Assets/Scripts/ClientSession.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using XXField;
+
+public class ClientSession : Gamnet.StreamSession
+{
+    public string host;
+    public int port;
+    public uint msgSEQ;
+    public int slowCount;
+    public float slowTime;
+    public float elapsedTime;
+    public float minResponseTime;
+    public float maxResponseTime;
+    private float lastSendTime;
+	public delegate void OnNetworkEvent();
+    public delegate void OnErrorEvent(System.Exception e);
+
+    public OnNetworkEvent onConnect;
+    public OnNetworkEvent onReconnect;
+    public OnNetworkEvent onClose;
+    public OnErrorEvent onError;
+
+    // Use this for initialization
+    void Start()
+    {
+        msgSEQ = 0;
+        slowCount = 0;
+        slowTime = 0.1f;
+        lastSendTime = 0.0f;
+        elapsedTime = 0.0f;
+        minResponseTime = float.MaxValue;
+        maxResponseTime = 0.0f;
+        Connect(host, port);
+
+
+        RegisterHandler(MsgSvrCli_Field_StressTest_Ans.MSG_ID, (Gamnet.Buffer buf) =>
+        {
+            MsgSvrCli_Field_StressTest_Ans ans = new MsgSvrCli_Field_StressTest_Ans();
+            if(false == ans.Load(buf))
+            {
+                throw new System.Exception("MsgSvrCli_Field_StressTest_Ans() load fail");
+            }
+
+            float responseTime = Time.realtimeSinceStartup - ans.SendTime;
+            if(minResponseTime > responseTime)
+            {
+                minResponseTime = responseTime;
+            }
+
+            if(maxResponseTime < responseTime)
+            {
+                maxResponseTime = responseTime;
+            }
+
+            if(slowTime < responseTime)
+            {
+                slowCount++;
+            }
+        });
+    }
+
+    bool stopSend = false;
+    float stopTime = 0.0f;
+    void Update()
+    {
+        base.Update();
+        elapsedTime += Time.deltaTime;
+        if(0 == Random.Range(0, 300))
+        {
+            stopSend = true;
+            return;
+        }
+
+        if (false == stopSend)
+        {
+            MsgCliSvr_Field_StressTest_Req req = new MsgCliSvr_Field_StressTest_Req();
+            req.MsgSEQ = msgSEQ++;
+            req.SendTime = Time.realtimeSinceStartup;
+
+            SendMsg(req);
+        }
+        else
+        {
+            stopTime += Time.deltaTime;
+            if(1.0f < stopTime)
+            {
+                stopSend = false;
+                stopTime = 0.0f;
+            }
+        }
+    }
+        
+    public override void OnConnect()
+    {
+    }
+    public override void OnReconnect()
+    {
+        
+    }
+    public override void OnClose()
+    {
+        
+    }
+    public override void OnError(System.Exception e)
+    {
+    }
+}
---
+++ Assets/Scripts/Network/Buffer.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace Gamnet {
+	public class Buffer
+	{
+		public static int BUFFER_SIZE = 8192;
+		public byte[] buffer = null;
+		
+		public int readIndex = 0;
+		public int writeIndex = 0;
+		public Buffer() 
+		{
+			buffer = new byte[BUFFER_SIZE];
+		}
+        public Buffer(byte[] src)
+		{
+			buffer = new byte[BUFFER_SIZE];
+            Copy(src);
+		}
+        public Buffer(System.IO.MemoryStream ms)
+        {
+            buffer = new byte[BUFFER_SIZE];
+            Copy(ms);
+        }
+        public Buffer(Buffer src)
+        {
+            buffer = new byte[BUFFER_SIZE];
+            Copy(src);
+        }        
+		public int Size()
+		{
+			return writeIndex - readIndex;
+		}
+		public int Available()
+		{
+			return BUFFER_SIZE - writeIndex;
+		}
+		public void Copy(byte[] src)
+		{
+			System.Buffer.BlockCopy (src, 0, buffer, 0, src.Length); 
+			writeIndex = src.Length;
+			readIndex = 0;
+		}
+		public void Copy(System.IO.MemoryStream ms)
+		{
+			System.Array.Copy(ms.GetBuffer(), 0, buffer, readIndex, ms.GetBuffer().Length);
+		}
+		public void Copy(Buffer src)
+		{
+            if(0 == src.Size())
+            {
+                return;
+            }
+			System.Buffer.BlockCopy(src.buffer, src.readIndex, buffer, 0, src.Size());
+			writeIndex = src.Size ();
+			readIndex = 0;
+		}
+		public void Append(Buffer src)
+		{
+			System.Buffer.BlockCopy(src.buffer, src.readIndex, buffer, writeIndex, src.Size());
+			writeIndex += src.Size ();
+		}
+		public static void BlockCopy (Buffer src, Buffer dest, int size)
+		{
+			System.Buffer.BlockCopy (src.buffer, src.readIndex, dest.buffer, 0, size);
+			dest.readIndex = 0;
+			dest.writeIndex = size;
+		}
+		public static implicit operator System.IO.MemoryStream(Gamnet.Buffer src)  // explicit byte to digit conversion operator
+		{
+			System.IO.MemoryStream ms = new System.IO.MemoryStream();
+			ms.Write(src.buffer, src.readIndex, src.Size());
+			ms.Seek(0, System.IO.SeekOrigin.Begin);
+			return ms;
+		}
+        public static implicit operator byte[](Gamnet.Buffer src)
+		{
+			byte [] dest = new byte[src.Size ()];
+			System.Buffer.BlockCopy (src.buffer, src.readIndex, dest, 0, src.Size ());
+			return dest;
+		}
+        public static Gamnet.Buffer operator + (Gamnet.Buffer lhs, Gamnet.Buffer rhs)
+        {
+            Buffer buffer = new Buffer();
+            buffer.Append(lhs);
+            buffer.Append(rhs);
+            return buffer;
+        }
+	}
+}
