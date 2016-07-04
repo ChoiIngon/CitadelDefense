542461849fdcfc72fc016e63b2109db672b53d69
ChoiIngon <kukuta@gmail.com>
2016-07-04 17:10:09 +0900

    직선 탄환 발사시 진행 방향에 따른 각도 변환 추가

--- Assets/Scripts/Unit/UnitMove_Stright.cs
+++ Assets/Scripts/Unit/UnitMove_Stright.cs
@@ -5,7 +5,19 @@ public class UnitMove_Stright : UnitMove {
 	public override void Init(Vector3 start, Vector3 end)
 	{
 		base.Init (start, end);
-	}
+
+        Vector3 from = end - start;
+        float angle = Vector3.Angle(from, new Vector3(from.x, 0.0f, 0.0f));
+        if (0.0f > from.y)
+        {
+            angle = 360 - angle;
+        }
+        if (0.0f > from.x)
+        {
+            angle = 180 - angle;
+        }
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angle));
+    }
 	void Update() {
 		if (1.0f < _interpolate) {
 			enabled = false;
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+    public Vector3 start;
+    public Vector3 end;
+	// Use this for initialization
+	void Start () {
+        Vector3 from = end - start;
+        float angle = Vector3.Angle(from, new Vector3(from.x, 0.0f, 0.0f));
+        if(end.y < start.y)
+        {
+            angle = 360 - angle;
+        }
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angle));
+        Debug.Log(angle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 from = end - start;
+        float angle = Vector3.Angle(from, new Vector3(from.x, 0.0f, 0.0f));
+        if (0.0f > from.y)
+        {
+            angle = 360 - angle;
+        }
+        if(0.0f > from.x)
+        {
+            angle = 180 - angle;
+        }
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, angle));
+    }
+}
