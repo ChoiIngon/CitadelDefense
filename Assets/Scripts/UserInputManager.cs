66a715db6b759b5b501df9bde0b7a07b8c938c44
kukuta <kukuta@gmail.com>
2016-07-07 03:59:04 +0900

    터치 위치 버그 수정 넓은 해상도로 고정

--- Assets/Scripts/UserInputManager.cs
+++ Assets/Scripts/UserInputManager.cs
@@ -24,6 +24,7 @@ public class UserInputManager : MonoBehaviour {
                 }
                 touchEvents.Add(touchEvent, touchEvent);
 			}
+			lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		}
         foreach(var v in touchEvents)
         {
