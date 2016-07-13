abbe854f7cd4cf559bdf355229fe820b53ad98cf
ChoiIngon <kukuta@gmail.com>
2016-07-13 19:13:17 +0900

    라이트닝 이펙트 추가

--- Assets/Scripts/Unit/UnitAttack_Lightning.cs
+++ Assets/Scripts/Unit/UnitAttack_Lightning.cs
@@ -1,55 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
+[RequireComponent(typeof(LineRenderer))]
 public class UnitAttack_Lightning : UnitAttack {
 	// Use this for initialization
 	public Vector3 start;
 	public Vector3 end;
-	private float distance;
-	private float angle;
-	public Sprite sprite;
-	public Vector3[] points;
-	public float lifeTime;
+    public float sparkTime;
+    public float width;
+	private Vector3[] points;
+
 	void Start () {
-		//SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer> ();
-		//spriteRenderer.sprite = sprite;
-		//ebug.Log (spriteRenderer.sprite.rect.size / spriteRenderer.sprite.pixelsPerUnit);
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.sortingLayerName = "Effect";
+        StartCoroutine(DrawLightning());
+    }
 
-		int pointCount = (int)distance * Random.Range (1, 3);
-		//points = new Vector3 [pointCount];
-		distance = Vector3.Distance (start, end);
-
-		LineRenderer lineRenderer = GetComponent<LineRenderer> ();
-		lineRenderer.sortingLayerName = "Effect";
-
-		points = new Vector3 [21];
-		Split (0, 20, 0, 10);
-		lineRenderer.SetVertexCount (21);
-		lineRenderer.SetPositions (points);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
+    IEnumerator DrawLightning()
+    {
+        //for(int i=0; i<20; i++)
+        while(true)
+        {
+            float distance = Vector3.Distance(start, end);
+            int verticeCount = (int)distance * Random.Range(1, 4) + 3;
+            points = new Vector3[verticeCount];
+            points[0] = start;
+            points[verticeCount - 1] = end;
+
+            SetVertices(0, verticeCount - 1, start, end);
+
+            LineRenderer lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.SetVertexCount(verticeCount);
+            lineRenderer.SetPositions(points);
+            lineRenderer.SetWidth(width, width);
+            yield return new WaitForSeconds(sparkTime);
+        }
+    }
 	public override void Attack()
 	{
 		
 	}
 
-	private void Split(int start, int end, float startPos, float endPos)
+	private void SetVertices(int start, int end, Vector3 startPos, Vector3 endPos)
 	{
 		int center = start + (end - start)/ 2; 
 		if (0 == center || points.Length - 1 == center) {
 			return;
 		}
-		points[center].y = Mathf.Lerp(startPos, endPos, Random.Range(0.4f, 0.6f));
-		Debug.Log ("index:" + center + ", start:" + startPos + ", end:" + endPos + ", pos:" + points [center].y);
+		Vector3 point = Vector3.Lerp(startPos, endPos, Random.Range(0.4f, 0.6f));
+        float displacement = Vector3.Distance(startPos, endPos) / 10;
+        points[center] = new Vector3(point.x + Random.Range(-displacement, displacement), point.y + Random.Range(-displacement, displacement), point.z);
 		if (0 == (end - start) / 2) {
 			return;
 		}
-		Split(start, center , startPos, points[center].y);
-		Split(center + 1, end, points[center].y, endPos);
+        SetVertices(start, center , startPos, points[center]);
+        SetVertices(center + 1, end, points[center], endPos);
 	}
 }
