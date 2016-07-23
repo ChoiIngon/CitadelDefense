732b3090a762a228942cd451a660ba6142ac9544
kukuta <kukuta@gmail.com>
2016-07-23 17:57:49 +0900

    요세 버프 UI 적용

---
+++ Assets/Scripts/UI/ContentCitadelBuff.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ContentCitadelBuff : MonoBehaviour {
+	[HideInInspector]
+	public CitadelBuff buff;
+	public PanelCitadelBuff panel;
+	public Image icon;
+	public Text level;
+	// Use this for initialization
+	void Start () {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+			panel.selectedBuff = buff;
+			panel.description.text = buff.info.description;
+        });
+
+		icon.sprite = buff.info.icon;
+		level.text = buff.level.ToString ();
+	}
+}
---
+++ Assets/Scripts/UI/PanelCitadelBuff.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PanelCitadelBuff : MonoBehaviour {
+	public ContentCitadelBuff contentPrefab;
+	public Transform content;
+	public Text description;
+	public Button levelup;
+	public CitadelBuff selectedBuff;
+	void Start()
+	{
+		levelup.onClick.AddListener (() => {
+			if(null != selectedBuff)
+			{
+				selectedBuff.Upgrade();
+			}
+		});
+		foreach(CitadelBuff buff in GameManager.Instance.citadel.citadelBuffs)
+		{
+			ContentCitadelBuff contentCitadelBuff = GameObject.Instantiate<ContentCitadelBuff>(contentPrefab);
+			contentCitadelBuff.panel = this;
+			contentCitadelBuff.buff = buff;
+			contentCitadelBuff.transform.SetParent(content);
+			contentCitadelBuff.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+		}
+	}
+}
