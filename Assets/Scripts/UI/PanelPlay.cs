4dc2d53517b4b97e51ff4116def4a78409f1fefb
kukuta <kukuta@gmail.com>
2016-09-26 08:50:18 +0900

    백 버튼 추가

--- Assets/Scripts/UI/PanelPlay.cs
+++ Assets/Scripts/UI/PanelPlay.cs
@@ -3,10 +3,12 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class PanelPlay : MonoBehaviour {
-	public Button buttonSpeedup;
+	public Button buttonBack;
+	public Button buttonSpeed;
+
 	// Use this for initialization
 	void Start () {
-		buttonSpeedup.onClick.AddListener (() => {
+		buttonSpeed.onClick.AddListener (() => {
 			if(GameManager.Instance.timeScale > 1.0f)
 			{
 				GameManager.Instance.timeScale = 1.0f;
@@ -14,6 +16,7 @@ public class PanelPlay : MonoBehaviour {
 			else
 			{
 				GameManager.Instance.timeScale = 2.0f;
+				buttonSpeed.Select();
 			}
 			Time.timeScale = GameManager.Instance.timeScale;
 		});
